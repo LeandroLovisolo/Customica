3277243a22fa889681102921eecb67ef2c35485a
Leandro Lovisolo <leandro@leandro.me>
2011-12-29 20:43:48 -0300

    Indentation fixes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -20,161 +20,161 @@ import play.templates.JavaExtensions;
 public class Application extends Controller {
 
 	private static final String LOGO_URL = "http://customica.com/images/logo.png";
-	
+
 	@Before
 	static void addReadOnlyFlag() {
 		renderArgs.put("readonly", isReadOnly());
 	}
-	
-    public static void index() {
-    	renderArgs.put("newestTShirts", TShirt.findLatest6());
-    	renderArgs.put("categories", Category.findAll());
-    	render();
-    }
-    
-    public static void category(Long id, String slug) {
-    	Category category = (Category) (id == null ? null : Category.findById(id));
-    	renderArgs.put("categories", Category.findAll());
-    	renderArgs.put("category", category);
-    	renderArgs.put("tShirts", category == null ? TShirt.findLatest() : category.getTShirts());
-    	render();
-    }
-    
-    public static void tshirt(Long id, String slug) {
-    	TShirt tShirt = TShirt.findById(id);
-    	if(tShirt == null) Application.index();
-    	
-    	// Open Graph tags
-    	renderArgs.put("ogTitle", tShirt.title);
-    	Map<String, Object> params = new HashMap<String, Object>();
-    	params.put("id", tShirt.id);
-    	params.put("slug", JavaExtensions.slugify(tShirt.title));
-    	renderArgs.put("ogUrl", Router.getFullUrl("Application.tshirt", params));
-    	renderArgs.put("ogImage", request.getBase() + tShirt.getDesignUrl());
-    	
-    	renderArgs.put("tShirt", tShirt);
-    	renderArgs.put("moreTShirtsInCategory", tShirt.category.getTop4TShirts());
-    	render();
-    }
-    
-    public static void buyButton(Long id, Gender gender, Size size) {
-    	failIfReadOnly();
-    	order(id, genderToUrlParam(gender), sizeToUrlParam(size));
-    }
-    
-    public static void order(Long id, String gender, String size) {
-    	failIfReadOnly();
-    	TShirt tShirt = TShirt.findById(id);
-    	Gender genderValue = genderFromUrlParam(gender);
-    	Size sizeValue = sizeFromUrlParam(size);
-    	if(tShirt == null || genderValue == null || sizeValue == null) Application.index();
-    	renderArgs.put("tShirt", tShirt);
-    	renderArgs.put("gender", genderValue);
-    	renderArgs.put("size", sizeValue);
-    	render();
-    }
-    
-    public static void submitOrder(Long id, Gender gender, Size size, @Valid Order order) {
-    	failIfReadOnly();
-    	TShirt tShirt = TShirt.findById(id);
-    	if(tShirt == null) Application.index();
-    	
-        if(validation.hasErrors()) {
-            params.flash();
-            validation.keep();
-            order(id, genderToUrlParam(gender), sizeToUrlParam(size));
-        } else {
-        	order.tShirt = tShirt;
-        	order.gender = gender;
-        	order.size = size;
-        	order.save();
-        	flash.put("orderId", order.id);
-        	redirectToDineroMail();
-        }
-    }
-    
-    public static void redirectToDineroMail() {
-    	Long orderId = null;
-    	try {
-    		orderId = Long.parseLong(flash.get("orderId"));
-    	} catch(NumberFormatException e) {
-    		Application.index();
-    	}
-    	
-    	Order order = Order.findById(orderId);
-    	if(order == null) Application.index();
-    	
-    	DineroMailHttpPost post = DineroMailHttpPostFactory.create(order, LOGO_URL,
-    			Router.getFullUrl("Application.thanks"), Router.getFullUrl("Application.index"));
 
-    	render(post);
-    }
-    
-    public static void thanks() {
-    	renderText("Gracias!");
-    }
-    
-    public static void designer(Long id) {
-    	if(id != null) {
-    		TShirt tShirt = TShirt.findById(id);
-    		if(tShirt == null) error("TShirt doesn't exist");
-    		if(!tShirt.isAuthorLoggedIn()) error("User is not author.");
-    		renderArgs.put("tShirt", tShirt);
-    		renderArgs.put("xml", new String(tShirt.xml).replace("\"", "\\\"").replace("\r", "\\r").replace("\n", "\\n"));
-    	}
-    	renderArgs.put("categories", Category.findAll());
-    	render();
-    }
-    
-    public static void submitDesign(Long id, String xml, String title, Long categoryId, boolean shareOnFacebook) {
-    	failIfReadOnly();
+	public static void index() {
+		renderArgs.put("newestTShirts", TShirt.findLatest6());
+		renderArgs.put("categories", Category.findAll());
+		render();
+	}
 
-    	if(xml == null || title == null || categoryId == null) error("Invalid input.");
-    	
-    	TShirt tShirt = null;
-    	if(id != null) tShirt = TShirt.findById(id);
-    	
-    	if(tShirt == null) {
-    		tShirt = TShirt.create(title, categoryId, xml.getBytes(), shareOnFacebook);
-    	} else {
-    		tShirt.update(title, categoryId, xml.getBytes());
-    	}
-    	
-    	tshirt(tShirt.id, JavaExtensions.slugify(tShirt.title));
-    }
-    
-    public static void deleteTShirt(Long id) {
-    	failIfReadOnly();
-    	
-    	TShirt tShirt = TShirt.findById(id);
-    	if(tShirt != null && tShirt.isAuthorLoggedIn()) tShirt.delete();
-    	Application.index();
-    }
-    
+	public static void category(Long id, String slug) {
+		Category category = (Category) (id == null ? null : Category.findById(id));
+		renderArgs.put("categories", Category.findAll());
+		renderArgs.put("category", category);
+		renderArgs.put("tShirts", category == null ? TShirt.findLatest() : category.getTShirts());
+		render();
+	}
+
+	public static void tshirt(Long id, String slug) {
+		TShirt tShirt = TShirt.findById(id);
+		if(tShirt == null) Application.index();
+
+		// Open Graph tags
+		renderArgs.put("ogTitle", tShirt.title);
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("id", tShirt.id);
+		params.put("slug", JavaExtensions.slugify(tShirt.title));
+		renderArgs.put("ogUrl", Router.getFullUrl("Application.tshirt", params));
+		renderArgs.put("ogImage", request.getBase() + tShirt.getDesignUrl());
+
+		renderArgs.put("tShirt", tShirt);
+		renderArgs.put("moreTShirtsInCategory", tShirt.category.getTop4TShirts());
+		render();
+	}
+
+	public static void buyButton(Long id, Gender gender, Size size) {
+		failIfReadOnly();
+		order(id, genderToUrlParam(gender), sizeToUrlParam(size));
+	}
+
+	public static void order(Long id, String gender, String size) {
+		failIfReadOnly();
+		TShirt tShirt = TShirt.findById(id);
+		Gender genderValue = genderFromUrlParam(gender);
+		Size sizeValue = sizeFromUrlParam(size);
+		if(tShirt == null || genderValue == null || sizeValue == null) Application.index();
+		renderArgs.put("tShirt", tShirt);
+		renderArgs.put("gender", genderValue);
+		renderArgs.put("size", sizeValue);
+		render();
+	}
+
+	public static void submitOrder(Long id, Gender gender, Size size, @Valid Order order) {
+		failIfReadOnly();
+		TShirt tShirt = TShirt.findById(id);
+		if(tShirt == null) Application.index();
+
+		if(validation.hasErrors()) {
+			params.flash();
+			validation.keep();
+			order(id, genderToUrlParam(gender), sizeToUrlParam(size));
+		} else {
+			order.tShirt = tShirt;
+			order.gender = gender;
+			order.size = size;
+			order.save();
+			flash.put("orderId", order.id);
+			redirectToDineroMail();
+		}
+	}
+
+	public static void redirectToDineroMail() {
+		Long orderId = null;
+		try {
+			orderId = Long.parseLong(flash.get("orderId"));
+		} catch(NumberFormatException e) {
+			Application.index();
+		}
+
+		Order order = Order.findById(orderId);
+		if(order == null) Application.index();
+
+		DineroMailHttpPost post = DineroMailHttpPostFactory.create(order, LOGO_URL,
+				Router.getFullUrl("Application.thanks"), Router.getFullUrl("Application.index"));
+
+		render(post);
+	}
+
+	public static void thanks() {
+		renderText("Gracias!");
+	}
+
+	public static void designer(Long id) {
+		if(id != null) {
+			TShirt tShirt = TShirt.findById(id);
+			if(tShirt == null) error("TShirt doesn't exist");
+			if(!tShirt.isAuthorLoggedIn()) error("User is not author.");
+			renderArgs.put("tShirt", tShirt);
+			renderArgs.put("xml", new String(tShirt.xml).replace("\"", "\\\"").replace("\r", "\\r").replace("\n", "\\n"));
+		}
+		renderArgs.put("categories", Category.findAll());
+		render();
+	}
+
+	public static void submitDesign(Long id, String xml, String title, Long categoryId, boolean shareOnFacebook) {
+		failIfReadOnly();
+
+		if(xml == null || title == null || categoryId == null) error("Invalid input.");
+
+		TShirt tShirt = null;
+		if(id != null) tShirt = TShirt.findById(id);
+
+		if(tShirt == null) {
+			tShirt = TShirt.create(title, categoryId, xml.getBytes(), shareOnFacebook);
+		} else {
+			tShirt.update(title, categoryId, xml.getBytes());
+		}
+
+		tshirt(tShirt.id, JavaExtensions.slugify(tShirt.title));
+	}
+
+	public static void deleteTShirt(Long id) {
+		failIfReadOnly();
+
+		TShirt tShirt = TShirt.findById(id);
+		if(tShirt != null && tShirt.isAuthorLoggedIn()) tShirt.delete();
+		Application.index();
+	}
+
 	private static String genderToUrlParam(Gender gender) {
 		return gender == Gender.MALE ? "varon" : "mujer";
 	}
-	
+
 	private static Gender genderFromUrlParam(String gender) {
 		if(gender == null) return null;
 		return gender.equals("varon") ? Gender.MALE : Gender.FEMALE;
 	}
-    
+
 	private static String sizeToUrlParam(Size size) {
 		return size.toString().toLowerCase();
 	}
-	
+
 	private static Size sizeFromUrlParam(String size) {
 		if(size == null) return null;
 		return Size.fromString(size.toUpperCase());
 	}
-	
+
 	private static boolean isReadOnly() {
 		return "true".equals(Play.configuration.getProperty("readonly").trim().toLowerCase());
 	}
-	
+
 	private static void failIfReadOnly() {
 		if(isReadOnly()) throw new RuntimeException("Action not available in read-only mode.");
 	}
-	
+
 }
--- app/models/FacebookService.java
+++ app/models/FacebookService.java
@@ -15,21 +15,21 @@ import com.restfb.exception.FacebookException;
 import com.restfb.types.FacebookType;
 
 public class FacebookService {
-	
+
 	public static final String APP_ID = Play.configuration.getProperty("facebook.id");
 	public static final String APP_SECRET = Play.configuration.getProperty("facebook.secret");
 	public static final String COOKIE_NAME = "fbsr_" + APP_ID;
-	
+
 	private static FacebookService instance;
 
 	public static FacebookService get() {
 		if(instance == null) instance = new FacebookService();
 		return instance;
 	}
-	
+
 	private FacebookService() {
 	}
-	
+
 	private String getCookie() {
 		if(Http.Request.current().cookies == null) {
 			return null;
@@ -40,37 +40,37 @@ public class FacebookService {
 			return null;
 		}
 	}
-	
+
 	private FacebookCookie getValidatedFacebookCookie() {
 		FacebookCookie cookie = new FacebookCookie(getCookie());
 		if(!cookie.validate(APP_SECRET)) return null;
 		return cookie;
 	}
-	
+
 	public Long getLoggedInUserId() {
 		FacebookCookie cookie = getValidatedFacebookCookie();
 		return cookie == null ? null : cookie.getUserId();
 	}
-	
+
 	private String getAccessToken() {
 		FacebookCookie cookie = getValidatedFacebookCookie();
 		if(cookie == null) return null;
 		WS.HttpResponse response = WS.url("https://graph.facebook.com/oauth/access_token?"
-                + "client_id=" + APP_ID
-                + "&redirect_uri="
-                + "&client_secret=" + APP_SECRET
-                + "&code=" + cookie.getCode()).get();
-		
+				+ "client_id=" + APP_ID
+				+ "&redirect_uri="
+				+ "&client_secret=" + APP_SECRET
+				+ "&code=" + cookie.getCode()).get();
+
 		for(String parameter : response.getString().split("&")) {
 			String pair[] = parameter.split("=");
 			if(pair.length > 1 && "access_token".equals(pair[0])) {
 				return pair[1];
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	private FacebookClient getFacebookClient() {
 		return new DefaultFacebookClient(getAccessToken());
 	}
@@ -80,35 +80,35 @@ public class FacebookService {
 		com.restfb.types.User user = facebookClient.fetchObject("me", com.restfb.types.User.class);
 		return user.getEmail();
 	}
-	
+
 	public void postToWall(String message, String picture, String link, String name, String caption, String description) {
 		FacebookClient facebookClient = getFacebookClient();		
 		try {
 			FacebookType response = facebookClient.publish(
 					"me/feed",
 					FacebookType.class,
-				    Parameter.with("message", message),
-				    Parameter.with("picture", picture),
-				    Parameter.with("link", link),
-				    Parameter.with("name", name),
-				    Parameter.with("caption", caption),
-				    Parameter.with("description", nonNull(description)));
+					Parameter.with("message", message),
+					Parameter.with("picture", picture),
+					Parameter.with("link", link),
+					Parameter.with("name", name),
+					Parameter.with("caption", caption),
+					Parameter.with("description", nonNull(description)));
 			Logger.debug("Published Facebook message ID: " + response.getId());
 		} catch (FacebookException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	private String nonNull(String string) {
 		return string == null ? "" : string;
 	}
-	
+
 	public boolean hasPublishStreamPermission() {
 		class Permissions {
 			@Facebook(value="publish_stream")
 			Integer publishStream;
 		}
-		
+
 		FacebookClient facebookClient = getFacebookClient();		
 		List<Permissions> results = null;
 		try {
@@ -120,5 +120,5 @@ public class FacebookService {
 		}
 		return results.get(0).publishStream == 1;
 	}
-	
+
 }
